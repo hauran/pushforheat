a30dc81d119d20926a83ae234a76441fb2610b45
rmai <richardmai@gmail.com>
2019-03-20 13:29:10 -0400

    update everything

---
+++ build/precache-manifest.09af15ea909cf1d3d0d2ce7edaed15c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bd3df0a253b07b2e006a",
+    "url": "/static/js/main.f87a4bb3.chunk.js"
+  },
+  {
+    "revision": "00accb05001171c80834",
+    "url": "/static/js/2.2f1339d1.chunk.js"
+  },
+  {
+    "revision": "bd3df0a253b07b2e006a",
+    "url": "/static/css/main.963ae83d.chunk.css"
+  },
+  {
+    "revision": "05770c42b8e4bfd21bc2f24f19f1fa6a",
+    "url": "/index.html"
+  }
+];
