4752c4d7ae1003018499b039397b4459c853e808
rmai <richardmai@gmail.com>
2019-05-13 00:55:46 -0400

    no service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import unregisterServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-unregisterServiceWorker()
