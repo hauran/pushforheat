c4ed391c2916f6b5b37421dbfacd2e6a9542c5ae
Richard Mai <richardmai@gmail.com>
2019-07-30 19:06:24 -0400

    Updates

---
+++ precache-manifest.aed278890449147d88427a99d4ddf9b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ac55f9995609addd9a1a96fe8d93246",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7acf4cbd47ac636dd6c7",
+    "url": "/static/css/main.dae87504.chunk.css"
+  },
+  {
+    "revision": "f5060c870a3cdd86a4e3",
+    "url": "/static/js/2.f5c43510.chunk.js"
+  },
+  {
+    "revision": "7acf4cbd47ac636dd6c7",
+    "url": "/static/js/main.619b7329.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
