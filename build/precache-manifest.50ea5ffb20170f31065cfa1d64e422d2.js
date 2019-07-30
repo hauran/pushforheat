9cc3690216b0b5a44467403171fbd19b52b61fed
Richard Mai <richardmai@gmail.com>
2019-07-30 18:47:05 -0400

    touch

---
+++ build/precache-manifest.50ea5ffb20170f31065cfa1d64e422d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2269f33f0a6d14d75116e43d7b43003d",
+    "url": "/pushforheat/index.html"
+  },
+  {
+    "revision": "7acf4cbd47ac636dd6c7",
+    "url": "/pushforheat/static/css/main.dae87504.chunk.css"
+  },
+  {
+    "revision": "714e16c8f62cd8b32713",
+    "url": "/pushforheat/static/js/2.24713c2e.chunk.js"
+  },
+  {
+    "revision": "7acf4cbd47ac636dd6c7",
+    "url": "/pushforheat/static/js/main.619b7329.chunk.js"
+  },
+  {
+    "revision": "03c4c74976e416f83468",
+    "url": "/pushforheat/static/js/runtime~main.3f3120de.js"
+  }
+]);
