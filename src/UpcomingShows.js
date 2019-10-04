616bd9a357d117a4a70f9c66db3fc106e68fde17
Richard Mai <richardmai@gmail.com>
2019-10-03 23:17:50 -0400

    spooktober 2019

--- src/UpcomingShows.js
+++ src/UpcomingShows.js
@@ -6,11 +6,12 @@ import Show from './Show'
 const shows = [
   {
     title: 'Spooktoberfest 2019',
+    link: './flyers/Spooktoberfest_2019.pdf',
     map:
       'https://www.google.com/maps/place/Hart%E2%80%99s+Brook+Park+and+Preserve/@41.0197724,-73.8077669,15z/data=!4m2!3m1!1s0x0:0x27e8a4f624b81cfd?ved=2ahUKEwjYytmzh5HhAhWonuAKHRWABqgQ_BIwEXoECAcQCA',
     venue: "Hart's Brook Park & Reserve",
     date: 'Saturday Oct 19th',
-    info: '2pm - 4pm'
+    info: '1pm - 3pm'
   },
 ]
 
